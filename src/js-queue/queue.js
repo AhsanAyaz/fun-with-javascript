84c340582b1566bda813b08082ebb1ad13fc9747
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-06-19 15:18:46 +0200

    js queue final code

--- src/js-queue/queue.js
+++ src/js-queue/queue.js
@@ -1,31 +1,96 @@
 import "../styles/globals.css";
+import "./js-queue.css";
 
 export class Queue {
-  items = [];
+  items = {};
+  count = 0;
+  itemToRemoveIndex = 0;
   add(item) {
-    this.items.push(item);
+    this.items[this.count] = item;
+    this.count++;
   }
 
   isEmpty() {
-    return this.items.length === 0;
+    return this.size() === 0;
   }
 
   clear() {
-    this.items.length = 0;
+    this.items = {};
+    this.count = 0;
+    this.itemToRemoveIndex = 0;
   }
 
-  takeOut() {
+  remove() {
     if (this.isEmpty()) {
       return undefined;
     }
-    return this.items.pop();
+    const itemToRemove = this.items[this.itemToRemoveIndex];
+    delete this.items[this.itemToRemoveIndex];
+    this.itemToRemoveIndex++;
+    return itemToRemove;
   }
 
   size() {
-    return this.items.length;
+    return this.count - this.itemToRemoveIndex;
   }
 
   peek() {
-    return this.items[this.items.length - 1];
+    return this.items[this.itemToRemoveIndex];
   }
 }
+
+const myQueue = new Queue();
+
+const peekQueueItem = () => {
+  const peekedElement = document.querySelector(".box_item:first-child");
+  if (!peekedElement) {
+    return;
+  }
+  peekedElement.classList.add("peeking");
+  setTimeout(() => {
+    peekedElement.classList.remove("peeking");
+  }, 500);
+};
+
+const initiateHandlers = () => {
+  const addItemBtn = document.querySelector("#addItemBtn");
+  const peekItemBtn = document.querySelector("#peekItemBtn");
+  const takeOutItemBtn = document.querySelector("#takeOutItemBtn");
+  const clearBtn = document.querySelector("#clearBtn");
+  clearBtn.addEventListener("click", () => {
+    myQueue.clear();
+    renderQueue();
+  });
+  addItemBtn.addEventListener("click", () => {
+    const randomNumber = Math.round(Math.random() * 50 + 1);
+    myQueue.add(randomNumber);
+    renderQueue();
+  });
+  takeOutItemBtn.addEventListener("click", () => {
+    myQueue.remove();
+    renderQueue();
+    peekQueueItem();
+  });
+  peekItemBtn.addEventListener("click", () => {
+    peekQueueItem();
+  });
+};
+
+const renderQueue = () => {
+  const queueElement = document.querySelector(".box");
+  queueElement.querySelectorAll(".box_item").forEach((item) => item.remove());
+  for (const key in myQueue.items) {
+    const item = myQueue.items[key];
+    const queueItemElement = document.createElement("DIV");
+    queueItemElement.classList.add("box_item");
+    queueItemElement.textContent = item;
+    queueElement.append(queueItemElement);
+  }
+};
+
+const main = () => {
+  initiateHandlers();
+  renderQueue();
+};
+
+main();
--- src/js-queue/queue.spec.js
+++ src/js-queue/queue.spec.js
@@ -8,7 +8,21 @@ describe("Queue", () => {
   });
   it("should add item to the queue", () => {
     queue.add(5);
-    expect(queue.items.length).toEqual(1);
+    expect(queue.size()).toEqual(1);
     expect(queue.peek()).toEqual(5);
+  });
+
+  it("should remove items in the sequence of FIFO from the queue", () => {
+    queue.add(5);
+    queue.add(6);
+    queue.add(7);
+    queue.add(8);
+
+    queue.remove();
+    queue.remove();
+
+    expect(queue.size()).toEqual(2);
+    expect(queue.peek()).toEqual(7);
+    expect(queue.isEmpty()).toBeFalsy();
   });
 });
