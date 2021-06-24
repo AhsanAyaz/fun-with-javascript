a5cd903331aa43f17f18003a09f45691e7146416
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-06-25 00:46:43 +0200

    js-dequeue starting state

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     js_queue: "./src/js-queue/queue.js",
     js_stack: "./src/js-stack-example/main.js",
+    js_dequeue: "./src/js-dequeue/dequeue.js",
   },
   devtool: "inline-source-map",
   plugins: [
@@ -19,6 +20,12 @@ module.exports = {
       title: "JS Stack",
       template: "./src/js-stack-example/index.html",
       filename: "./js_stack/index.html",
+      inject: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: "JS DeQueue",
+      template: "./src/js-dequeue/index.html",
+      filename: "./js_dequeue/index.html",
       inject: false,
     }),
   ],
