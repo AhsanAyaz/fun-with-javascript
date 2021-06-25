ec1a246d61f4b4743bc5b65b8b4938267b35ef26
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-06-25 20:33:29 +0200

    dequeue start

--- src/js-dequeue/dequeue.js
+++ src/js-dequeue/dequeue.js
@@ -1,21 +1,3 @@
-import "../styles/globals.css";
-import "./dequeue.css";
+class DeQueue {}
 
-export class DeQueue {}
-
-const renderDeQueue = (items = []) => {
-  const boxElement = document.querySelector(".box");
-  boxElement.querySelectorAll(".box_item").forEach((item) => item.remove());
-  items.forEach((item) => {
-    const itemElement = document.createElement("DIV");
-    itemElement.classList.add("box_item");
-    itemElement.textContent = item;
-    boxElement.append(itemElement);
-  });
-};
-
-const main = () => {
-  renderDeQueue(new Array(12).fill(0).map((_, index) => index + 1));
-};
-
-main();
+export default DeQueue;
