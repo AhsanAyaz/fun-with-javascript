a88827db47e449f4ab0278895306896ea3303df2
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-07-12 01:05:46 +0200

    lazy-load-images starter files

--- src/lazy-load-images/script.js
+++ src/lazy-load-images/script.js
@@ -1,6 +1,19 @@
 import './styles.css';
+import CardItem from './CardItem';
+
+const generateImages = () => {
+  const container = document.querySelector('.app-container');
+  const sections = container.querySelectorAll('section');
+  for (let i = 0, len = sections.length; i < len; ++i) {
+    const section = sections[i];
+    const columns = section.querySelectorAll('.col-md-6');
+    const targetColumn = i % 2 === 0 ? columns[1] : columns[0];
+    targetColumn.innerHTML = CardItem(i);
+  }
+};
 const main = () => {
   console.log('JS is awesome!');
+  generateImages();
 };
 
 main();
