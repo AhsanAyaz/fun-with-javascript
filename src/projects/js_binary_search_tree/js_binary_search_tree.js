38764e4f734650a1a60a5a41f02c12f357780487
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-12-07 14:13:07 +0100

    wip: binary search tree UI

--- src/projects/js_binary_search_tree/js_binary_search_tree.js
+++ src/projects/js_binary_search_tree/js_binary_search_tree.js
@@ -14,10 +14,18 @@ const defaultCompareFn = (a, b) => {
 
 class TreeNode {
   constructor(value, parent) {
-    this.value = value;
+    this.value = value.toString();
     this.parent = parent || null;
     this.left = null;
     this.right = null;
+  }
+
+  get isLeaf() {
+    return this.left === null && this.right === null;
+  }
+
+  get hasChildren() {
+    return !this.isLeaf;
   }
 }
 class BinarySearchTree {
@@ -29,40 +37,139 @@ class BinarySearchTree {
   }
 
   insert(value) {
-    if (this.root === null) {
+    let node = this.root;
+    if (node === null) {
       this.root = new TreeNode(value);
-    } else {
-      this.insertTreeNode(value, this.root);
+      return this.root;
+    }
+    const nodeInserted = (() => {
+      while (true) {
+        const comparison = this.compareFn(value, node.value);
+        if (comparison === COMPARISON.EQUAL) {
+          return false;
+        }
+        if (comparison === COMPARISON.SMALLER) {
+          if (node.left === null) {
+            node.left = new TreeNode(value, node);
+            return true;
+          } else {
+            node = node.left;
+          }
+        } else if (comparison === COMPARISON.GREATER) {
+          if (node.right === null) {
+            node.right = new TreeNode(value, node);
+            return true;
+          } else {
+            node = node.right;
+          }
+        }
+      }
+    })();
+    if (nodeInserted) {
+      return node;
     }
   }
 
-  insertTreeNode(value, parentNode) {
-    const comparison = this.compareFn(value, parentNode.value);
-    if (comparison === COMPARISON.EQUAL) {
-      return;
-    }
-    if (comparison === COMPARISON.SMALLER) {
-      if (!parentNode.left) {
-        parentNode.left = new TreeNode(value, parentNode);
+  remove(value, node) {
+    node = node ? node : this.search(value);
+    if (!node) return null;
+
+    const nodeIsRoot = node.parent === null;
+    const hasBothChildren = node.left !== null && node.right !== null;
+    const isLeftChild = !nodeIsRoot ? node.parent.left === node : false;
+
+    if (node.isLeaf) {
+      if (nodeIsRoot) {
+        this.root = null;
       } else {
-        this.insertTreeNode(value, parentNode.left);
+        if (node.parent.left === node) {
+          node.parent.left = null;
+        } else {
+          node.parent.right = null;
+        }
       }
+      return node;
+    } else if (!hasBothChildren) {
+      const child = node.left !== null ? node.left : node.right;
+      if (nodeIsRoot) {
+        this.root = child;
+      } else {
+        if (isLeftChild) {
+          node.parent.left = child;
+        } else {
+          node.parent.right = child;
+        }
+      }
+      child.parent = node.parent;
+      return node;
     } else {
-      if (!parentNode.right) {
-        parentNode.right = new TreeNode(value, parentNode);
+      const minRightLeaf = this.min(node.right);
+      console.log('minRightLeaf', minRightLeaf);
+      if (minRightLeaf.parent.left === minRightLeaf) {
+        minRightLeaf.parent.left = null;
       } else {
-        this.insertTreeNode(value, parentNode.right);
+        minRightLeaf.parent.right = null;
       }
+      node.value = minRightLeaf.value;
+      return node;
     }
   }
-
-  remove(value) {}
-  search(value) {}
-  min() {}
-  max() {}
-  inOrderTraverse() {}
-  preOrderTraverse() {}
-  postOrderTraverse() {}
+  search(value) {
+    return this.postOrderTraverse().find((node) => node.value === value);
+  }
+  min(node = this.root) {
+    let current = node;
+    while (current !== null && current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+  max(node = this.root) {
+    let current = node;
+    while (current !== null && current.right !== null) {
+      current = current.right;
+    }
+    return current;
+  }
+  inOrderTraverse(node = this.root, traversed = []) {
+    if (node === null) {
+      return traversed;
+    }
+    if (node.left) {
+      traversed.push(...this.inOrderTraverse(node.left));
+    }
+    traversed.push(node);
+    if (node.right) {
+      traversed.push(...this.inOrderTraverse(node.right));
+    }
+    return traversed;
+  }
+  preOrderTraverse(node = this.root, traversed = []) {
+    if (node === null) {
+      return traversed;
+    }
+    traversed.push(node);
+    if (node.left) {
+      traversed.push(...this.preOrderTraverse(node.left));
+    }
+    if (node.right) {
+      traversed.push(...this.preOrderTraverse(node.right));
+    }
+    return traversed;
+  }
+  postOrderTraverse(node = this.root, traversed = []) {
+    if (node === null) {
+      return traversed;
+    }
+    if (node.left) {
+      traversed.push(...this.postOrderTraverse(node.left));
+    }
+    if (node.right) {
+      traversed.push(...this.postOrderTraverse(node.right));
+    }
+    traversed.push(node);
+    return traversed;
+  }
 }
 
 export default BinarySearchTree;
--- src/projects/js_binary_search_tree/main.js
+++ src/projects/js_binary_search_tree/main.js
@@ -2,6 +2,7 @@ import '@styles/globals.css';
 import '@styles/tree.css';
 import './js_binary_search_tree.css';
 import BinarySearchTree from './js_binary_search_tree';
+import initiateHandlers from './bst-ui';
 
 export const renderBinarySearchTree = (node) => {
   const { left, right, value } = node;
@@ -36,14 +37,36 @@ myTree.insert(6);
 myTree.insert(9);
 myTree.insert(8);
 myTree.insert(10);
-myTree.insert(14);
+myTree.insert(15);
+console.log(myTree.insert(13));
 myTree.insert(12);
-myTree.insert(13);
+myTree.insert(14);
+myTree.insert(20);
+myTree.insert(18);
+myTree.insert(25);
+console.log('inOrderTraverse');
+console.log(myTree.inOrderTraverse());
+console.log('preOrderTraverse');
+console.log(myTree.preOrderTraverse());
+console.log('postOrderTraverse');
+console.log(myTree.postOrderTraverse());
+console.log('min', myTree.min());
+console.log('max', myTree.max());
+console.log(myTree.search(12));
+
+const reRender = (tree, render) => {
+  const treeDOMElement = document.querySelector('.tree');
+  treeDOMElement.innerHTML = render(tree.root);
+};
 
 const main = () => {
   console.log('treeData', myTree);
+  reRender(myTree, renderBinarySearchTree);
   const treeDOMElement = document.querySelector('.tree');
   treeDOMElement.innerHTML = renderBinarySearchTree(myTree.root);
+  initiateHandlers(myTree, () => {
+    reRender(myTree, renderBinarySearchTree);
+  });
 };
 
 main();
